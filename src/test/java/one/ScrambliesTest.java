ee092eab81d5b1d243cb7d64162b6738163f0d57
bill <bill.zhang@infinox.cn>
2018-09-07 18:17:29 +0800

    jjs

--- src/test/java/one/ScrambliesTest.java
+++ src/test/java/one/ScrambliesTest.java
@@ -20,7 +20,7 @@ public class ScrambliesTest {
         testing(Scramblies.scramble("scriptingjava","javascript"),true);
         testing(Scramblies.scramble("scriptsjava","javascripts"),true);
         testing(Scramblies.scramble("javscripts","javascript"),false);
-        testing(Scramblies.scramble("aabbcamaomsccdd","commas111"),true);
+        testing(Scramblies.scramble("aabbcamaomsccdd","cos111"),true);
         testing(Scramblies.scramble("aabbcamaomsccdd","112"),true);
         testing(Scramblies.scramble("commas","commas"),true);
         testing(Scramblies.scramble("sammoc","commas"),true);
