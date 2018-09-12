704b2b1cbfa4f100f4c63a556cf5aefa03bf7fa2
bill <bill.zhang@infinox.cn>
2018-09-12 09:30:58 +0800

    add sout

--- src/main/java/com/zero/one/DRoot.java
+++ src/main/java/com/zero/one/DRoot.java
@@ -5,6 +5,7 @@ public class DRoot {
     public static int digital_root(int n) {
         while(n > 9){
             n = n/10 + n % 10;
+            System.out.println("ddd");
         }
         return(n);
     }
