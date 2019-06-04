b7bc07b7291d03140eb21ab00d9a0ffd2b50019c
bill <bill.zhang@infinox.cn>
2019-06-04 17:51:27 +0800

    完善反向链表

--- src/main/java/com/linklist/ListTest.java
+++ src/main/java/com/linklist/ListTest.java
@@ -4,21 +4,28 @@ public class ListTest {
 
     public static void main(String[] args) {
 
-        //封装测试参数
+        //准备测试数据
         ListNode pre = null;
         for (int i =9;i>0;i--){
             ListNode temp = new ListNode(i);
             temp.next = pre;
             pre = temp;
         }
+        ListNode cur = pre;
+        System.out.println("正向遍历数据");
+        while (cur !=null){
+            System.out.print("->"+cur.val);
+            cur =cur.next;
+        }
 
         //反向链表
         ListNode node = SolutionOfReverseList.reverseList(pre);
-
+        System.out.println();
         //打印反向链表
+        System.out.println("反向遍历数据");
         ListNode tempNode = node;
         while (tempNode !=null){
-            System.out.println(tempNode.val);
+            System.out.print("->"+tempNode.val);
             tempNode =tempNode.next;
         }
     }
